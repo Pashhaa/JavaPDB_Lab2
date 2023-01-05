74c55defeb89ae23594c47ec58f7ea52e147afa4
Pashhaa <71789749+Pashhaa@users.noreply.github.com>
2023-01-05 16:33:47 +0300

    (fix) variable finalized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int bookNum = 201;
+        final int bookNum = 201;
         System.out.println("c5 - " + bookNum % 5); // C = Bт
         System.out.println("c7 - " + bookNum % 7); // char
         System.out.println("c11 - " + bookNum % 11 + "\n"); // Обчислити суму найбільших елементів кожного рядка матриці
